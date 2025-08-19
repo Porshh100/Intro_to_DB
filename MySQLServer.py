d72f386f510253d20d471581b1102f144cab29a1
portia dhladla <mimieportiadhladla@gmail.com>
2025-08-19 16:49:38 +0200

    fix: added correct MySQLServer.py and removed wrong file

--- MySQLServer.py
+++ MySQLServer.py
@@ -5,20 +5,21 @@ try:
     
     connection = mysql.connector.connect(
         host="localhost",
-        user="root",
-        password=""
+        user="root",       
+        password=""        
     )
 
     if connection.is_connected():
         cursor = connection.cursor()
+        
         cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store;")
         print("Database 'alx_book_store' created successfully!")
 
-except Error as e:
-    print(f"Error: {e}")
+except mysql.connector.Error as err:
+    print(f"Error: {err}")
 
 finally:
-    if connection.is_connected():
+    if 'connection' in locals() and connection.is_connected():
         cursor.close()
         connection.close()
-        print("MySQL connection is closed.")
+
